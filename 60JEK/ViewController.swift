2a61ffca050df3626ec1286456d750285f4f45dd
Nanda <nandamoji@gmail.com>
2019-05-20 11:15:33 +0700

    Initial Commit

---
+++ 60JEK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  60JEK
+//
+//  Created by Dr. Nanda L Prasetya, MMSc (MedEd) on 5/20/19.
+//  Copyright © 2019 Dr. Nanda L Prasetya, MMSc (MedEd). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
